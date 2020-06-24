facaf3b62c4dd63a00c0d80b89ecb4b611a04f76
Eric <eeoneric@gmail.com>
2020-06-24 13:49:09 -0400

    add filter

--- mail_app/management/commands/follow_up_mailer.py
+++ mail_app/management/commands/follow_up_mailer.py
@@ -7,19 +7,21 @@ from django.conf import settings
 
 from posthog.models import Event, Team
 from datetime import datetime
+from dateutil.relativedelta import relativedelta
 import pytz
 
 class Command(BaseCommand):
     help = 'Check user statuses and send email if necessary'
 
     def handle(self, *args, **options):
-        all_teams = Team.objects.all()
+        date_from = datetime.now(pytz.utc) - relativedelta(days=3)
+        all_teams = Team.objects.filter(created_at__gte=date_from).all()
         for team in all_teams:
             team_has_events = Event.objects.filter(team=team).exists()
             count = FollowUpEmail.objects.filter(team_id=team.pk).count()
             delta = datetime.now(pytz.utc) - team.created_at
 
-            if not team_has_events and delta > 2 and team.created_at and count < 1:
+            if not team_has_events and delta.days >= 2 and team.created_at and count < 1:
                 self._send_follow_up_to_team(team, count)
 
     def _send_follow_up_to_team(self, team: Team, count: int):
