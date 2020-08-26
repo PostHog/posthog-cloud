fa6bbd56608620a44b09adbea985f6eea623fdb4
Paolo D'Amico <paolodamico@users.noreply.github.com>
2020-08-26 09:08:23 -0500

    feat(messaging): test check_and_send_event_ingestion_follow_up

--- messaging/mail.py
+++ messaging/mail.py
@@ -1,6 +1,7 @@
-from django.core.mail import send_mail
-from django.conf import settings
 from typing import ClassVar
+
+from django.conf import settings
+from django.core.mail import send_mail
 
 
 class Mail:
