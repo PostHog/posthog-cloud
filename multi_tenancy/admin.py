d35ed92a320a33b654df8753bad6a77bc421c91e
Michael Matloka <dev@twixes.com>
2020-09-09 14:11:26 +0200

    Remove TeamBillingAdmin

--- multi_tenancy/admin.py
+++ multi_tenancy/admin.py
@@ -1,27 +1,8 @@
 from django.contrib import admin
 
 from .models import (
-    TeamBilling, OrganizationBilling
+    OrganizationBilling
 )
-
-@admin.register(TeamBilling)
-class TeamBillingAdmin(admin.ModelAdmin):
-    readonly_fields = ("stripe_checkout_session",)
-    list_display = (
-        "get_team_name",
-        "stripe_customer_id",
-        "should_setup_billing",
-        "billing_period_ends",
-        "price_id",
-    )
-
-    def get_queryset(self, request):
-        qs = super().get_queryset(request)
-        return qs.order_by("should_setup_billing")
-
-    def get_team_name(self, obj):
-        return obj.team.name
-
 
 @admin.register(OrganizationBilling)
 class OrganizationBillingAdmin(admin.ModelAdmin):
