d1b1059cbb8a702e7756c995402a1487ca1b6592
Yakko Majuri <yakko.majuri@gmail.com>
2020-09-09 09:06:04 +0000

    made endpoints more flexible

--- multi_tenancy/urls.py
+++ multi_tenancy/urls.py
@@ -34,21 +34,39 @@ urlpatterns += posthog_urls[:-1]
 urlpatterns += [
     path(
         "billing/setup", stripe_checkout_view, name="billing_setup",
+    ),  
+    path(
+        "billing/setup/", stripe_checkout_view, name="billing_setup",
     ),  # Redirect to Stripe Checkout to set-up billing (requires session ID)
     path(
         "billing/manage", stripe_billing_portal, name="billing_manage",
+    ),  
+    path(
+        "billing/manage/", stripe_billing_portal, name="billing_manage",
     ),  # Redirect to Stripe Customer Portal to manage subscription
     path(
         "billing/welcome", billing_welcome_view, name="billing_welcome",
+    ),  
+    path(
+        "billing/welcome/", billing_welcome_view, name="billing_welcome",
     ),  # Page with success message after setting up billing
     path(
         "billing/failed", billing_failed_view, name="billing_failed",
+    ), 
+    path(
+        "billing/failed/", billing_failed_view, name="billing_failed",
     ),  # Page with failure message after attempting to set up billing
     path(
         "billing/hosted", billing_hosted_view, name="billing_hosted",
+    ),  
+    path(
+        "billing/hosted/", billing_hosted_view, name="billing_hosted",
     ),  # Page with success message after setting up billing for hosted plans
     path(
         "billing/stripe_webhook", stripe_webhook, name="billing_stripe_webhook",
+    ),  
+    path(
+        "billing/stripe_webhook/", stripe_webhook, name="billing_stripe_webhook",
     ),  # Stripe Webhook
     re_path(
         r"^.*", decorators.login_required(home),
