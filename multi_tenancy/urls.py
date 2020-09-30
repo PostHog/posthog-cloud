b6d3512613f146631f922d4a5abf571c58992d39
Michael Matloka <dev@twixes.com>
2020-09-30 10:48:14 +0200

    Revert "Revert "Revert "Try with opt_slash_path"""
    This reverts commit dad012c10421ec0343b12a2d353270f3fd486d1e.

--- multi_tenancy/urls.py
+++ multi_tenancy/urls.py
@@ -60,7 +60,7 @@ urlpatterns += [
     opt_slash_path(
         "plans", PlanViewset.as_view({"get": "list"}), name="billing_plans",
     ),
-    opt_slash_path(
+    path(
         "plans/<str:key>",
         PlanViewset.as_view({"get": "retrieve"}),
         name="billing_plan",
